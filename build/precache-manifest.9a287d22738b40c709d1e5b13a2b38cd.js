5041e97b04db3608f524620163c415f2c324fcf3
Matthew Morgan <mmorgan55@cnm.edu>
2019-11-08 14:17:11 -0700

    Updated branch .gitignore

---
+++ build/precache-manifest.9a287d22738b40c709d1e5b13a2b38cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd6fa2365d045a69c333bf2e76240b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fc9d3372b89521b9a842",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5bcb019b2c3ce58a2951",
+    "url": "/portfolio/static/js/2.bcd42778.chunk.js"
+  },
+  {
+    "revision": "fc9d3372b89521b9a842",
+    "url": "/portfolio/static/js/main.8ee591e4.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
