1d91b4395717765a94e27abc8b03ee791b5b531c
Matthew Morgan <mmorgan55@cnm.edu>
2019-11-08 20:29:49 -0700

    Updates

--- precache-manifest.0d7d1c52e3e4b3079aea087c40504d48.js
+++ precache-manifest.0d7d1c52e3e4b3079aea087c40504d48.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c9e393cd78c151b2adcfac1428c6a4b",
+    "revision": "9f1ce4ba851e56df4785e6d404e5ed9b",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "f60f288dcd43f20e31e4",
-    "url": "/portfolio/static/css/main.748c36e9.chunk.css"
+    "revision": "f448828357000fe26be5",
+    "url": "/portfolio/static/css/main.242b6f21.chunk.css"
   },
   {
     "revision": "9f052b21338a120321cd",
     "url": "/portfolio/static/js/2.f7658163.chunk.js"
   },
   {
-    "revision": "f60f288dcd43f20e31e4",
-    "url": "/portfolio/static/js/main.12ef71c6.chunk.js"
+    "revision": "f448828357000fe26be5",
+    "url": "/portfolio/static/js/main.76bcfa6a.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
