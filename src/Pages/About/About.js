0f3d83be5cd5a6ea6492f0de7d7e9dcd521e6a8a
Matthew Morgan <mmorgan55@cnm.edu>
2020-01-14 15:05:50 -0700

    More base styling. Will be improved when base website is done.

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,14 +7,16 @@ const About = () => {
     return (
         <div className="AboutMePage">
             <Card id="AboutMeImage">
-                <Image src="https://scx2.b-cdn.net/gfx/news/hires/2019/redwhitebutr.jpg" fluid thumbnail/>
+                <Card.Title style={{ textAlign: 'center', fontSize: '48px' }}>
+                    ABOUT ME
+                </Card.Title>
+                <Card.Body>
+                    <Image src="https://scx2.b-cdn.net/gfx/news/hires/2019/redwhitebutr.jpg" fluid thumbnail />
+                </Card.Body>
             </Card>
 
             <Card id="AboutMeText">
-                <Card.Title style={{textAlign: 'center',  fontSize: '48px'}}>
-                    ABOUT ME
-                </Card.Title>
-                <Card.Body style={{fontSize: '20px'}}>
+                <Card.Body style={{ fontSize: '20px' }}>
                     About me text shall go here at some point in the near future.
                 </Card.Body>
             </Card>
