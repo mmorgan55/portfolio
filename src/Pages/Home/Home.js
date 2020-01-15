c76fd5cd69fe335d692b579fd7cfd705e255fd60
Matthew Morgan <mmorgan55@cnm.edu>
2020-01-15 11:51:37 -0700

    Updated packages

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,25 +6,7 @@ const Home = (props) => {
   return (
     <div className="Home">
     
-      <div id="Title">
-        {/* <CardPort text={"Matthew Morgan Portfolio"} /> */}
-      </div>
-
-      <div id="About">
-        {/* <CardPort text={"About Me"} /> */}
-      </div>
-
-      <div id="Resume">
-        {/* <CardPort text={"Resume"} /> */}
-      </div>
-
-      <div id="Projects">
-        {/* <CardPort text={"Projects"} /> */}
-      </div>
-
-      <div id="Other">
-        {/* <CardPort text={"Contact"} /> */}
-      </div>
+      <p>HOME PAGE</p>
 
     </div>
   );
