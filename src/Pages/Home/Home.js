55cfeae72830d0fd1206a6e7785eb6a2cbfcfbda
Matthew Morgan <mmorgan55@cnm.edu>
2020-01-21 13:24:21 -0700

    Set base layout for homepage.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import './Home.css';
 
 const Home = (props) => {
@@ -6,7 +8,26 @@ const Home = (props) => {
   return (
     <div className="Home">
     
-      <p>HOME PAGE</p>
+      <p id="Title">HOME PAGE</p>
+
+      <Image id="HomeImage" src="https://scx2.b-cdn.net/gfx/news/hires/2019/redwhitebutr.jpg" fluid thumbnail />
+
+      <Button id="About" variant="primary">
+        About Me
+      </Button>
+
+      <Button id="Resume" variant="primary">
+        Resume
+      </Button>
+
+      <Button id="Projects" variant="primary">
+        Projects
+      </Button>
+
+      <Button id="Contact" variant="primary">
+        Contact
+      </Button>
+
 
     </div>
   );
